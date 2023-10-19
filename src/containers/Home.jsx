014837aea7cb072239d4fdc36978f1ffcae9c8c5
Rohan Singh <rohannsingh98765@gmail.com>
2023-10-20 00:23:33 +0530

    setting up api query and search logic

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,18 +3,21 @@ import Header from '../components/Header'
 import Images from '../components/Images'
 import { ImageContext } from '../App'
 
+
 const Home = () => {
 
-    const {res} = useContext(ImageContext)
+    const { res } = useContext(ImageContext)
 
     return (
         <div>
             <Header />
 
-            <div>
-                {res.map((data, key) =>
-                    <Images key={key} data={data} />
-                )}
+            <div className=' p-5 md:p-10'>
+                <div className='columns-2 md:columns-3 gap-6'>
+                    {res.map((data, key) =>
+                        <Images key={key} data={data} />
+                    )}
+                </div>
             </div>
         </div>
     )
